a4c4ed4c966e28b5539a3036cde9871af0f5f663
michael white <michaeljwhite@gmail.com>
2019-02-25 09:28:57 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,32 @@
+class Book
+  attr_accessor :genre
 
+  def initialize(title)
+    @title = title
+  end
 
+  def title
+    @title
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+  def page_count=(page_count)
+    @page_count = page_count
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def turn_page
+    puts 'Flipping the page...wow, you read fast!'
+  end
+
+end
