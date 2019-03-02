c04c04a751e1b5ccd8f2ec138a89dab116ef8104
chilbert <chase@chasehilbert.com>
2019-03-01 18:30:13 -0600

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,35 +1,10 @@
 class Book
 
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+
   def initialize(title)
     @title = title
-  end
-
-  def title
-    @title
-  end
-
-  def author=(author_name)
-    @author = author_name
-  end
-
-  def author
-    @author
-  end
-
-  def page_count=(pages)
-    @pages = pages
-  end
-
-  def page_count
-    @pages
-  end
-
-  def genre=(genre_name)
-    @genre = genre_name
-  end
-
-  def genre
-    @genre
   end
 
   def turn_page
