1319c8659ddcfd1e28eaa86db5c2e5e0e3728cab
chilbert <chase@chasehilbert.com>
2019-03-01 18:28:18 -0600

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,39 @@
+class Book
 
+  def initialize(title)
+    @title = title
+  end
 
+  def title
+    @title
+  end
+
+  def author=(author_name)
+    @author = author_name
+  end
+
+  def author
+    @author
+  end
+
+  def page_count=(pages)
+    @pages = pages
+  end
+
+  def page_count
+    @pages
+  end
+
+  def genre=(genre_name)
+    @genre = genre_name
+  end
+
+  def genre
+    @genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
