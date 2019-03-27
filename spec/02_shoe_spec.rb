b6d7ec5c3e666ef3586d0fadbbfb6be1ea90280b
Jennifer Joyce <jenjoyce@mac.com>
2019-03-27 03:34:31 +0000

    Done.

--- spec/02_shoe_spec.rb
+++ spec/02_shoe_spec.rb
@@ -1,3 +1,60 @@
+class Book
+  
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def brand 
+    @brand 
+  end
+  
+  def size(size)
+    @size = size
+  end
+  
+  def size
+    @size
+  end
+  
+  def color(color)
+    @color = color
+  end
+  
+  def color
+    @color
+  end
+  
+  def material(material)
+    @material = material
+  end
+  
+  def material
+    @material
+  end
+  
+  def condition(condition)
+    @condition = condition
+  end
+  
+  def condition
+    @condition
+  end
+  
+  
+  def cobbler
+    puts "The shoe has been repaired"
+  end
+  
+end
+
+
+
+
+
+
+
+
+
 describe 'Shoe' do
   let(:shoe) { Shoe.new("Nike") }
 
